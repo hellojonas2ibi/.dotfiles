f44f3be872e714c4d48c93686e79e98512d7bb5e
Hello Jonas <hello.jonas@2ibi.com>
2026-05-11 11:52:26 +0200

    add nvim-navic

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -1,6 +1,73 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-    }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "material",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			always_show_tabline = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+				refresh_time = 16, -- ~60fps
+				events = {
+					"WinEnter",
+					"BufEnter",
+					"BufWritePost",
+					"SessionLoadPost",
+					"FileChangedShellPost",
+					"VimResized",
+					"Filetype",
+					"CursorMoved",
+					"CursorMovedI",
+					"ModeChanged",
+				},
+			},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = {},
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {
+			lualine_b = {
+				{ "filename", path = 0 },
+			},
+			lualine_c = {
+				{
+					"navic",
+					color_correction = nil,
+					navic_opts = nil,
+				},
+			},
+		},
+		inactive_winbar = {
+			lualine_b = {
+				{ "filename", path = 0 },
+			},
+		},
+		extensions = {},
+	},
 }
